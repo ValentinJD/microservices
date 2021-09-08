84770ddc09baa2a78380481a9165e54698f84b34
ValentinJD <47910332+ValentinJD@users.noreply.github.com>
2021-09-08 20:37:38 +0400

    Комит на гите

--- consumer/src/main/java/consumer/demo/consumer/MessageListener.java
+++ consumer/src/main/java/consumer/demo/consumer/MessageListener.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageListener {
-
+// комит на гите
     @KafkaListener(topics="msg2", groupId = "demo.1")
     public void msgListener(ConsumerRecord<String, String> record){
         System.out.println(record.topic() + ' ' + record.value());
-    }
+    } 
 }
