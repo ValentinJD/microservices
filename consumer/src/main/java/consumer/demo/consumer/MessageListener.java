257d9f2e72bee8b3da28b814773da0736a5908ac
Валентин <moysait1984@yandex.ru>
2021-09-08 20:31:40 +0400

    Init

---
+++ consumer/src/main/java/consumer/demo/consumer/MessageListener.java
@@ -0,0 +1,16 @@
+package consumer.demo.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MessageListener {
+
+    @KafkaListener(topics="msg2", groupId = "demo.1")
+    public void msgListener(ConsumerRecord<String, String> record){
+        System.out.println(record.topic() + ' ' + record.value());
+    }
+}
